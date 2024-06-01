ca04e31554d54bf85c9f17c40bf48fc0f22eb254
Mustapha Khlil <mostaphakhlil5@gmail.com>
2024-06-01 17:28:42 +0100

    add spring ai

---
+++ src/main/java/com/example/rh_cv_automatisation/ai_management/utils/AppConfig.java
@@ -0,0 +1,14 @@
+package com.example.rh_cv_automatisation.ai_management.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
